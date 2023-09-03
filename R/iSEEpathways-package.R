b2b855ac215bb76735862d557ce80a7bc6616cd4
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-09-03 20:54:58 +0100

    add package man page

---
+++ R/iSEEpathways-package.R
@@ -0,0 +1,18 @@
+#' iSEE extension for panels related to pathway analysis
+#'
+#' `iSEEpathways` is a package that provides panels for \pkg{iSEE},
+#' facilitating the interactive visualisation of pathway analysis results.
+#'
+#' @author Kevin Rue-Albrecht \email{kevin.rue-albrecht@@imm.ox.ac.uk}
+#'
+#' @name iSEEpathways-pkg
+#' @docType package
+#' @keywords internal
+#'
+#' @examples
+#' library("iSEEde")
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
